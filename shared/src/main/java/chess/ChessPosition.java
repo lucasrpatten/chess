1b9b40afaf953996f3d325c009ef2a15ead3be39
lucasrpatten <lucasrp@byu.edu>
2024-06-26 15:15:11 -0600

    Implemented ChessPosition getters

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -22,13 +22,13 @@ public class ChessPosition {
      * @return which row this position is in 1 codes for the bottom row
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
      * @return which column this position is in 1 codes for the left row
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return col;
     }
 }

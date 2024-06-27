a8a893091cb665506d034d881543f2f47dae9354
lucasrpatten <lucasrp@byu.edu>
2024-06-27 10:39:57 -0600

    Added pawnMoves and bishopMoves helper functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,10 @@
 package chess;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.function.Predicate;
+
+import chess.ChessGame.TeamColor;
 
 /**
  * Represents a single chess piece
@@ -44,11 +48,121 @@ public class ChessPiece {
     /**
      * Calculates all the positions a chess piece can move to Does not take into
      * account moves that are illegal due to leaving the king in danger
-     *
+     * 
+     * @param board      The current chess board
+     * @param myPosition The pieces current position
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        switch (pieceType) {
+        case PAWN:
+            return pawnMoves(board, myPosition);
+        case BISHOP:
+            return bishopMoves(board, myPosition);
+        case KNIGHT:
+        case KING:
+        case QUEEN:
+        case ROOK:
+        default:
+            throw new RuntimeException(String.format("Unknown piece type: %s", pieceType.toString()));
+        }
+    }
+
+    /**
+     * Calculates a pawns valid moves
+     * 
+     * @param board      The current chess board
+     * @param myPosition The pieces current position
+     * @return HashSet of valid moves
+     */
+    private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        HashSet<ChessMove> moves = new HashSet<>();
+        int currentRow = myPosition.getRow();
+        int currentCol = myPosition.getColumn();
+        // // Check to ensure the pawn won't (somehow) be moving off the board
+        // if ((teamColor == TeamColor.WHITE && currentRow == 7) || (teamColor ==
+        // TeamColor.WHITE && currentRow == 1)) {
+        // return moves;
+        // }
+        int startRow = (teamColor == TeamColor.WHITE) ? 2 : 7;
+        int direction = (teamColor == TeamColor.WHITE) ? 1 : -1;
+
+        // Move Up
+        ChessPosition upOne = new ChessPosition(currentRow + direction, currentCol);
+        if (board.getPiece(upOne) == null) {
+            moves.add(new ChessMove(myPosition, upOne));
+
+            // Allowed to move up two if on starting row
+            if (currentRow == startRow) {
+                ChessPosition upTwo = new ChessPosition(currentRow + 2 * direction, currentCol);
+                if (board.getPiece(upTwo) == null) {
+                    moves.add(new ChessMove(myPosition, upTwo));
+                }
+            }
+        }
+
+        // Capture diagonally left
+        ChessPosition captureLeft = new ChessPosition(currentRow + direction, currentCol - 1);
+        ChessPiece pieceLeft = board.getPiece(captureLeft);
+        if (pieceLeft != null && pieceLeft.getTeamColor() != teamColor) {
+            moves.add(new ChessMove(myPosition, captureLeft));
+        }
+
+        // Capture diagonally right
+        ChessPosition captureRight = new ChessPosition(currentRow + direction, currentCol + 1);
+        ChessPiece pieceRight = board.getPiece(captureRight);
+        if (pieceRight != null && pieceRight.getTeamColor() != teamColor) {
+            moves.add(new ChessMove(myPosition, captureRight));
+        }
+
+        return moves;
+    }
+
+    /**
+     * Calculates a bishops valid moves
+     * 
+     * @param board      The current chess board
+     * @param myPosition The pieces current position
+     * @return HashSet of valid moves
+     */
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
+        HashSet<ChessMove> moves = new HashSet<>();
+        int currentRow = myPosition.getRow();
+        int currentCol = myPosition.getColumn();
+        int[][] directions = { { 1, 1 }, // up-right
+                { 1, -1 }, // up-left
+                { -1, 1 }, // down-right
+                { -1, -1 } // down-left
+        };
+
+        for (int[] direction : directions) {
+            int rowChange = direction[0];
+            int colChange = direction[1];
+            int row = currentRow;
+            int col = currentCol;
+            while (true) {
+                if (row < 1 || row > 8 || col < 1 || col > 8) {
+                    break;
+                }
+
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(newPosition);
+                // if empty allowed to move there
+                if (piece == null) {
+                    moves.add(new ChessMove(myPosition, newPosition));
+                }
+                else {
+                    // If enemy piece there, capture
+                    if (piece.teamColor != teamColor) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                    }
+                    // Can't jump over piece
+                    break;
+                }
+            }
+        }
+
+        return moves;
     }
 
     @Override

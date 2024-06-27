7a9e047c6406ba54524f39d8902955efe542e4b0
lucasrpatten <lucasrp@byu.edu>
2024-06-27 09:49:12 -0600

    Added ChessMove Constructors, Getters, and Overrides

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,29 +1,49 @@
 package chess;
 
+import chess.ChessPiece.PieceType;
+
 /**
  * Represents moving a chess piece on a chessboard
- * <p>
- * Note: You can add to this class, but you may not alter
- * signature of the existing methods.
  */
 public class ChessMove {
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+    private PieceType promotionPiece;
 
-    public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
-                     ChessPiece.PieceType promotionPiece) {
+    /**
+     * @param startPosition  Starting Position
+     * @param endPosition    Ending Position
+     * @param promotionPiece Type of piece to promote a pawn to if pawn promotion is
+     *                       part of the chess move
+     */
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition, ChessPiece.PieceType promotionPiece) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = promotionPiece;
+    }
+
+    /**
+     * @param startPosition Starting Position
+     * @param endPosition   Ending Position
+     */
+    public ChessMove(ChessPosition startPosition, ChessPosition endPosition) {
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.promotionPiece = null;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPosition;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPosition;
     }
 
     /**
@@ -33,6 +53,43 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotionPiece;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((startPosition == null) ? 0 : startPosition.hashCode());
+        result = prime * result + ((endPosition == null) ? 0 : endPosition.hashCode());
+        result = prime * result + ((promotionPiece == null) ? 0 : promotionPiece.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ChessMove other = (ChessMove) obj;
+        if (startPosition == null) {
+            if (other.startPosition != null)
+                return false;
+        }
+        else if (!startPosition.equals(other.startPosition))
+            return false;
+        if (endPosition == null) {
+            if (other.endPosition != null)
+                return false;
+        }
+        else if (!endPosition.equals(other.endPosition))
+            return false;
+        if (promotionPiece != other.promotionPiece)
+            return false;
+        return true;
+    }
+
 }

7bfa8e2166dcb2aaaad5ac8537ad125922194525
lucasrpatten <lucasrpattendev@gmail.com>
2024-07-28 12:00:07 -0600

    Added Sql data accesser

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -28,7 +28,8 @@ public class DatabaseManager {
                 var port = Integer.parseInt(props.getProperty("db.port"));
                 CONNECTION_URL = String.format("jdbc:mysql://%s:%d", host, port);
             }
-        } catch (Exception ex) {
+        }
+        catch (Exception ex) {
             throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
         }
     }
@@ -36,36 +37,37 @@ public class DatabaseManager {
     /**
      * Creates the database if it does not already exist.
      */
-    static void createDatabase() throws DataAccessException {
+    public static void createDatabase() throws DataAccessException {
         try {
             var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
             var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
             try (var preparedStatement = conn.prepareStatement(statement)) {
                 preparedStatement.executeUpdate();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
     }
 
     /**
      * Create a connection to the database and sets the catalog based upon the
-     * properties specified in db.properties. Connections to the database should
-     * be short-lived, and you must close the connection when you are done with it.
-     * The easiest way to do that is with a try-with-resource block.
-     * <br/>
+     * properties specified in db.properties. Connections to the database should be
+     * short-lived, and you must close the connection when you are done with it. The
+     * easiest way to do that is with a try-with-resource block. <br/>
      * <code>
      * try (var conn = DbInfo.getConnection(databaseName)) {
      * // execute SQL statements.
      * }
      * </code>
      */
-    static Connection getConnection() throws DataAccessException {
+    public static Connection getConnection() throws DataAccessException {
         try {
             var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
             conn.setCatalog(DATABASE_NAME);
             return conn;
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
     }
---
+++ server/src/main/java/dataaccess/sql/SqlDAO.java
@@ -0,0 +1,90 @@
+package dataaccess.sql;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.google.gson.Gson;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+
+public abstract class SqlDAO {
+    private static boolean isConfigured = false;
+
+    public SqlDAO() throws DataAccessException {
+        configure();
+    }
+
+    protected <T> T query(String s, Parser<T> parser, Object... args) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+                PreparedStatement statement = conn.prepareStatement(s)) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i] instanceof ChessGame) {
+                    statement.setString(i + 1, new Gson().toJson(args[i]));
+                }
+                else {
+                    statement.setObject(i + 1, args[i]);
+                }
+            }
+
+            try (ResultSet res = statement.executeQuery()) {
+                return parser.parse(res);
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    protected int update(String s, Object... args) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+                PreparedStatement statement = conn.prepareStatement(s, Statement.RETURN_GENERATED_KEYS)) {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i] instanceof ChessGame) {
+                    statement.setString(i + 1, new Gson().toJson(args[i]));
+                }
+                else {
+                    statement.setObject(i + 1, args[i]);
+                }
+            }
+
+            statement.executeUpdate();
+
+            try (ResultSet res = statement.getGeneratedKeys()) {
+                if (res.next()) {
+                    return res.getInt(1);
+                }
+                return 0;
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private void configure() throws DataAccessException {
+        if (!isConfigured) {
+            DatabaseManager.createDatabase();
+            isConfigured = true;
+        }
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : getStatements()) {
+                conn.createStatement().execute(statement);
+            }
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @FunctionalInterface
+    protected interface Parser<T> {
+        T parse(ResultSet resultSet) throws SQLException;
+    }
+
+    protected abstract String[] getStatements();
+}
